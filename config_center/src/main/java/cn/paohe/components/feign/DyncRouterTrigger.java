35bc09f296d0473ee63b2a87fd8ea9eb255ee84a
huangzhimin <huangzhimin@paohe.com.cn>
2020-09-25 19:04:39 +0800

    调整下

--- config_center/src/main/java/cn/paohe/components/feign/DyncRouterTrigger.java
+++ config_center/src/main/java/cn/paohe/components/feign/DyncRouterTrigger.java
@@ -3,7 +3,7 @@ package cn.paohe.components.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "dync-gateway", url = "http://127.0.0.1:12001")
+@FeignClient(value = "dync-gateway")
 public interface DyncRouterTrigger {
 
     @GetMapping("/dync/getRouterReflush")
